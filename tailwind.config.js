5efa40e3dbe79cb2656b6f19548283716cf5fe7f
Duong Thu Huong <huong.duong22403@gmail.com>
2024-06-04 10:27:20 +0700

    Update nav-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          seashell: '#fff5ec',
+          metalic_bronze: '#A96942',
+        },
+      },
     },
+    variants: {},
     plugins: [],
   }
   
