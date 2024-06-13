9b37f654699ac519ee5afc4140781c8d216fcc5e
NguyenVanNhi <nhi.nguyen220406@vnuk.edu.vn>
2024-06-13 08:39:19 +0700

    Update about us (final 2)

---
+++ src/scripts/about_us.js
@@ -0,0 +1,29 @@
+function toggleSlides() {
+    const slide1 = document.querySelector('.review-slide-1');
+    const slide2 = document.querySelector('.review-slide-2');
+    
+    if (slide1.classList.contains('hidden')) {
+      slide1.classList.remove('hidden');
+      slide1.classList.add('flex');
+      slide2.classList.remove('flex');
+      slide2.classList.add('hidden');
+    } else {
+      slide1.classList.remove('flex');
+      slide1.classList.add('hidden');
+      slide2.classList.remove('hidden');
+      slide2.classList.add('flex');
+    }
+  }
+  function startSlideInterval() {
+    intervalId = setInterval(toggleSlides, 1000);
+  }
+
+  function stopSlideInterval() {
+    clearInterval(intervalId);
+  }
+
+  document.getElementById('review-container').addEventListener('mouseover', stopSlideInterval);
+  document.getElementById('review-container').addEventListener('mouseout', startSlideInterval);
+
+  // Start the interval when the page loads
+  startSlideInterval();
