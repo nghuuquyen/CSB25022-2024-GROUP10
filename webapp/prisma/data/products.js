f75d2997a20ebc2dcd42bcf12d9314c0e4d5320f
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-06-23 02:00:52 +0700

    Implement database schema

---
+++ webapp/prisma/data/products.js
@@ -0,0 +1,164 @@
+export default [
+    {
+        image: '/img/hot_cake_1.png',
+        name: 'Chocolate Cupcake',
+        description:
+            'A chocolate cupcake is a rich, single-serving chocolate cake topped with creamy chocolate frosting.',
+        regular_price: '25000',
+        sale_price: '20000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/hot_cake_2.png',
+        name: 'Blueberry Cupcake',
+        description:
+            'A blueberry cupcake is a moist, single-serving cake studded with juicy blueberries and often topped with a sweet, creamy frosting.',
+        regular_price: '20000',
+        sale_price: '15000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/new_cake_1.png',
+        name: 'Tiramisu',
+        description:
+            'Tiramisu is incredibly delicious, with its rich, creamy layers and a perfect balance of coffee and cocoa flavors, making it a favorite among dessert lovers.',
+        regular_price: '85000',
+        sale_price: '70000',
+        category: 'Tiramisu',
+    },
+    {
+        image: '/img/new_cake_2.png',
+        name: 'Macaron',
+        description:
+            'A macaron is a delicate French pastry featuring two almond meringue cookies sandwiched around a flavorful filling such as buttercream, ganache, or jam.',
+        regular_price: '100000',
+        sale_price: '90000',
+        category: 'Macaroon',
+    },
+    {
+        image: '/img/menu1.png',
+        name: 'Banana Bread',
+        description:
+            "Banana bread is a moist, sweet loaf made with ripe bananas, often including walnuts or chocolate chips. It's perfect for breakfast, snacks, or dessert.",
+        regular_price: '60000',
+        sale_price: '50000',
+        category: 'Banana cake',
+    },
+    {
+        image: '/img/menu2.png',
+        name: 'Whole wheat Bread',
+        description:
+            "Whole wheat bread is a nutritious loaf made from whole wheat flour, known for its hearty texture and nutty flavor. It's a healthy alternative to white bread.",
+        regular_price: '55000',
+        sale_price: '50000',
+        category: 'Banana cake',
+    },
+    {
+        image: '/img/menu3.png',
+        name: 'Chocolate cake',
+        description:
+            'Chocolate cake is a rich, moist dessert made with chocolate-flavored sponge layers and creamy chocolate frosting, ideal for celebrations.',
+        regular_price: '35000',
+        sale_price: '30000',
+        category: 'Banana cake',
+    },
+    {
+        image: '/img/menu4.png',
+        name: 'Strawberry Cake',
+        description:
+            'Strawberry cake is a fruity dessert made with fresh or pureed strawberries, offering a light and refreshing taste, often topped with strawberry frosting.',
+        regular_price: '35000',
+        sale_price: '30000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/menu5.png',
+        name: 'Chocolate cookies',
+        description:
+            'Chocolate cookies are a treat made with chocolate-flavored dough and chocolate chips or chunks, known for their chewy or crispy texture and rich chocolate taste.',
+        regular_price: '50000',
+        sale_price: '45000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/menu6.png',
+        name: 'Orange Cupcake',
+        description:
+            'Orange cupcakes are light, citrus-flavored treats with a moist texture, often topped with creamy orange frosting. They offer a refreshing twist on classic cupcakes.',
+        regular_price: '15000',
+        sale_price: '10000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/menu7.png',
+        name: 'Coffee Cupcake',
+        description:
+            'Coffee cupcakes are rich, coffee-infused treats with a tender crumb, typically adorned with coffee or mocha frosting. Perfect for coffee lovers.',
+        regular_price: '25000',
+        sale_price: '20000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/menu8.png',
+        name: 'Blackberry Cupcake',
+        description:
+            'Blackberry cupcakes are fruity delights made with fresh blackberries, offering a sweet-tart flavor, often topped with blackberry-infused frosting.',
+        regular_price: '20000',
+        sale_price: '15000',
+        category: 'Cupcake',
+    },
+    {
+        image: '/img/menu9.png',
+        name: 'Passion Fruit Mousse',
+        description:
+            'Passion fruit mousse is a creamy, tropical dessert made with fresh passion fruit, offering a tangy and sweet flavor, light and airy in texture.',
+        regular_price: '55000',
+        sale_price: '50000',
+        category: 'Mousse',
+    },
+    {
+        image: '/img/menu10.png',
+        name: 'Chocolate Mousse',
+        description:
+            'Chocolate mousse is a decadent, airy dessert made with rich chocolate, offering a smooth and velvety texture, perfect for chocolate lovers.',
+        regular_price: '55000',
+        sale_price: '50000',
+        category: 'Mousse',
+    },
+    {
+        image: '/img/menu11.png',
+        name: 'Peach Mousse',
+        description:
+            'Peach mousse is a light, fruity dessert made with fresh peaches, delivering a sweet and refreshing flavor with a smooth and creamy texture.',
+        regular_price: '55000',
+        sale_price: '50000',
+        category: 'Mousse',
+    },
+    {
+        image: '/img/menu12.png',
+        name: 'Fruit Cheesecake',
+        description:
+            'Fruit cheesecake is a rich, creamy dessert with a graham cracker crust, topped with a variety of fresh fruits, offering a blend of tangy and sweet flavors.',
+        regular_price: '65000',
+        sale_price: '60000',
+        category: 'Cheesecake',
+    },
+    {
+        image: '/img/menu13.png',
+        name: 'Croissant',
+        description:
+            'A croissant is a buttery, flaky pastry with a crisp outer layer and a soft, airy interior, perfect for breakfast or snacks, often enjoyed plain or with fillings.',
+        regular_price: '25000',
+        sale_price: '20000',
+        category: 'Banana cake',
+    },
+    {
+        image: '/img/menu14.png',
+        name: 'Donut',
+        description:
+            'A donut is a sweet, fried dough treat, typically ring-shaped or filled, available in various flavors and toppings like glaze, sprinkles, or sugar.',
+        regular_price: '25000',
+        sale_price: '20000',
+        category: 'Banana cake',
+    },
+];
---
+++ webapp/prisma/migrations/20240622185633_initial_database/migration.sql
@@ -0,0 +1,94 @@
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `category_id` INTEGER NULL,
+    `description` VARCHAR(191) NULL,
+    `regular_price` DECIMAL(65, 30) NOT NULL,
+    `sale_price` DECIMAL(65, 30) NOT NULL,
+    `quantity_in_stock` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `categories_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `customers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NULL DEFAULT '',
+    `address` VARCHAR(191) NULL DEFAULT '',
+    `city` VARCHAR(191) NULL DEFAULT '',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `orders` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `order_date` DATETIME(3) NOT NULL,
+    `total_amount` DECIMAL(65, 30) NULL,
+    `customer_id` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `order_details` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quantity` INTEGER NOT NULL,
+    `price` DECIMAL(65, 30) NOT NULL,
+    `order_id` INTEGER NULL,
+    `product_id` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `message` VARCHAR(191) NOT NULL,
+    `customer_id` INTEGER NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order_details` ADD CONSTRAINT `order_details_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order_details` ADD CONSTRAINT `order_details_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `contacts` ADD CONSTRAINT `contacts_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
