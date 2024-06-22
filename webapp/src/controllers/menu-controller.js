b6b56921b4f89346039a1b6740dea285eb18ad52
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-06-23 02:50:05 +0700

    Implement logic backend for menu page

--- webapp/src/controllers/menu-controller.js
+++ webapp/src/controllers/menu-controller.js
@@ -1,5 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const getMenu = async (req, res) => {
-    return res.render('pages/menu');
+    const { page = 1, category, search, sort } = req.query;
+    const itemsPerPage = 10;
+    let where = {};
+    let orderBy = {};
+
+    if (category) {
+        where.categoryId = parseInt(category);
+    }
+
+    if (search) {
+        where.name = {
+            contains: search,
+        };
+    }
+
+    if (sort) {
+        switch (sort) {
+            case 'new':
+                orderBy = { createdAt: 'desc' };
+                break;
+            case 'popular':
+                orderBy = { views: 'desc' };
+                break;
+            case 'priceDesc':
+                orderBy = { salePrice: 'desc' };
+                break;
+            case 'priceAsc':
+                orderBy = { salePrice: 'asc' };
+                break;
+        }
+    }
+
+    const totalItems = await prisma.product.count({ where });
+
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const products = await prisma.product.findMany({
+        where,
+        orderBy,
+        skip: (page - 1) * itemsPerPage,
+        take: itemsPerPage,
+    });
+
+    const categories = await prisma.category.findMany({});
+
+    return res.render('pages/menu', {
+        categories,
+        products,
+        currentPage: parseInt(page),
+        totalPages,
+        category,
+        search,
+        sort,
+    });
 };
 
 export { getMenu };
