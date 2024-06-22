978dfc4b3a98835eaab56999e1ee034dd160f150
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2024-06-23 04:58:43 +0700

    Update vite config

--- mockup/vite.config.js
+++ mockup/vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: process.env.VITE_BASE_PATH || '/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                menu: 'menu.html',
+                cart: 'cart.html',
+                checkout: 'checkout.html',
+                thank_you: 'thank_you.html',
+            }
+        }
+    }
 });
